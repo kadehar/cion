a071d7956145892e8877062f028b759c4386f35e
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-10-31 10:14:56 +0300

    fix hideUi add showUi

--- app/src/main/java/com/github/kadehar/cion/feature/movie_player_screen/ui/MoviePlayerFragment.kt
+++ app/src/main/java/com/github/kadehar/cion/feature/movie_player_screen/ui/MoviePlayerFragment.kt
@@ -2,7 +2,11 @@ package com.github.kadehar.cion.feature.movie_player_screen.ui
 
 import android.annotation.SuppressLint
 import android.view.View
+import android.view.WindowInsetsController
 import androidx.core.os.bundleOf
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.kadehar.cion.R
@@ -51,6 +55,7 @@ class MoviePlayerFragment : Fragment(R.layout.fragment_movie_player) {
 
     override fun onPause() {
         super.onPause()
+        showSystemUi()
         if (Util.SDK_INT < 24) {
             releasePlayer()
         }
@@ -58,6 +63,7 @@ class MoviePlayerFragment : Fragment(R.layout.fragment_movie_player) {
 
     override fun onStop() {
         super.onStop()
+        showSystemUi()
         if (Util.SDK_INT >= 24) {
             releasePlayer()
         }
@@ -66,23 +72,42 @@ class MoviePlayerFragment : Fragment(R.layout.fragment_movie_player) {
     private fun initializePlayer() {
         player = SimpleExoPlayer.Builder(requireContext())
             .build().also { videoPlayer ->
-            binding.videoPlayerView.player = videoPlayer
-            videoPlayer.setMediaItem(MediaItem.fromUri(url))
-            videoPlayer.playWhenReady = playWhenReady
-            videoPlayer.seekTo(currentWindow, playbackPosition)
-            videoPlayer.prepare()
-        }
+                binding.videoPlayerView.player = videoPlayer
+                videoPlayer.setMediaItem(MediaItem.fromUri(url))
+                videoPlayer.playWhenReady = playWhenReady
+                videoPlayer.seekTo(currentWindow, playbackPosition)
+                videoPlayer.prepare()
+            }
     }
 
     @SuppressLint("InlinedApi")
     private fun hideSystemUi() {
-        binding.videoPlayerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                )
+        WindowCompat.setDecorFitsSystemWindows(
+            requireActivity().window,
+            false
+        )
+
+        WindowInsetsControllerCompat(
+            requireActivity().window,
+            binding.videoPlayerView
+        ).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun showSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(
+            requireActivity().window,
+            true
+        )
+
+        WindowInsetsControllerCompat(
+            requireActivity().window,
+            binding.videoPlayerView
+        ).show(WindowInsetsCompat.Type.systemBars())
     }
 
     private fun releasePlayer() {
